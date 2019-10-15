e988f4513b6f8ca1463672b4f9b64e3c5bb256e8
skadmsfl <noeytt.pk@gmail.com>
2019-10-15 15:00:14 +0700

    Look

--- app/src/main/java/com/example/kotlintutorial/Loot.kt
+++ app/src/main/java/com/example/kotlintutorial/Loot.kt
@@ -5,4 +5,7 @@ enum class LootType {
 }
 
 class Loot(val name: String, val type: LootType, val value: Double) {
+    override fun toString(): String {
+        return "$name is $type and is worth $value"
+    }
 }
