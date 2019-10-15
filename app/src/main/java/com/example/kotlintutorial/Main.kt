5b7a828d97ccebeaa3e9b44acbdef8897f8f105f
skadmsfl <noeytt.pk@gmail.com>
2019-10-15 12:58:43 +0700

    Tim Buchalka

--- app/src/main/java/com/example/kotlintutorial/Main.kt
+++ app/src/main/java/com/example/kotlintutorial/Main.kt
@@ -3,4 +3,13 @@ package com.example.kotlintutorial
 fun main(args: Array<String>) {
     println("Hello World")
     println("My first Kotlin program")
+
+    var tim: String
+    tim = "Tim Buchalka"
+    println(tim)
+
+    var timSalary: Int = 16
+    var monthly: Int = timSalary * 3
+    println(timSalary)
+    println(monthly)
 }
