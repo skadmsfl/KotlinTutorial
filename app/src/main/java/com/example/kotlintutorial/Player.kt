7936b3f9cd66ad8d221172288905654f4aed68a4
skadmsfl <noeytt.pk@gmail.com>
2019-10-15 14:15:57 +0700

    Player

--- app/src/main/java/com/example/kotlintutorial/Player.kt
+++ app/src/main/java/com/example/kotlintutorial/Player.kt
@@ -1,6 +1,7 @@
 package com.example.kotlintutorial
 
-class Player (val name: String, var level: Int = 1, var lives: Int = 3, var score: Int = 0) {
+class Player(val name: String, var level: Int = 1, var lives: Int = 3, var score: Int = 0) {
+    var weapon: Weapon = Weapon("Fist", 1)
 
     fun show() {
         println("""
