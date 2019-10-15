6ba532030ec6aa634b5fcebc2a50a923cef0cb59
skadmsfl <noeytt.pk@gmail.com>
2019-10-15 15:41:06 +0700

    open fun

--- app/src/main/java/com/example/kotlintutorial/Enemy.kt
+++ app/src/main/java/com/example/kotlintutorial/Enemy.kt
@@ -2,15 +2,23 @@ package com.example.kotlintutorial
 
 open class Enemy(val name: String, var hitPoints: Int, var lives: Int) {
 
-    fun takeDamage(damage: Int) {
+    open fun takeDamage(damage: Int) {
         val remainingHitPoints = hitPoints - damage
         if (remainingHitPoints > 0) {
             hitPoints = remainingHitPoints
             println("$name took $damage points of damage, and has $hitPoints left")
         } else {
             lives -= 1
-            println("$name lost a life")
+            if (lives >0) {
+                println("$name lost a life")
+            } else {
+                println("No lives left, $name is dead")
+            }
         }
+    }
+
+    override fun toString(): String {
+        return "Name: $name, Hitpoints: $hitPoints, Lives: $lives"
     }
 }
 
