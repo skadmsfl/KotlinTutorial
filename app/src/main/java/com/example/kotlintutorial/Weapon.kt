26bc4eac2877d1940155d0d37024e199de1311f5
skadmsfl <noeytt.pk@gmail.com>
2019-10-15 15:00:36 +0700

    override fun

--- app/src/main/java/com/example/kotlintutorial/Weapon.kt
+++ app/src/main/java/com/example/kotlintutorial/Weapon.kt
@@ -2,4 +2,8 @@ package com.example.kotlintutorial
 
 class Weapon(val name: String, var damageInflicted: Int) {
 
+    override fun toString(): String {
+        return "$name inflicts $damageInflicted points of damage"
+    }
+
 }
