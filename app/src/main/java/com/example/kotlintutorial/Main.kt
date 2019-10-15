02c3ae56e5fb7b8cfc92f1be62f60fc3be76957c
skadmsfl <noeytt.pk@gmail.com>
2019-10-15 13:35:22 +0700

    How old are you:

--- app/src/main/java/com/example/kotlintutorial/Main.kt
+++ app/src/main/java/com/example/kotlintutorial/Main.kt
@@ -11,4 +11,25 @@ fun main(args: Array<String>) {
     } else {
         println("You're still alive!")
     }
-}
+
+    println("How old are you: ")
+    val age = readLine()!!.toInt()
+    println("age is $age")
+
+    var message: String
+    message = when {
+        age < 18 -> "You're too young to vote"
+        age == 100 -> "Congratulations"
+        else -> "You can vote"
+    }
+
+    message = if (age < 18) {
+        "You're too young to vote"
+    } else if (age == 100) {
+        "Congratulations"
+    } else {
+        "You can vote"
+    }
+
+        println(message)
+    }
