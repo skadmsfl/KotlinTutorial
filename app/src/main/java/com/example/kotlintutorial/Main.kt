bf3d29437b77c09353352fa6f6e23fca61afc2d9
skadmsfl <noeytt.pk@gmail.com>
2019-10-15 15:34:54 +0700

    Main

--- app/src/main/java/com/example/kotlintutorial/Main.kt
+++ app/src/main/java/com/example/kotlintutorial/Main.kt
@@ -1,5 +1,17 @@
 package com.example.kotlintutorial
 
 fun main(args: Array<String>) {
+    val enemy = Enemy("test enemy", 10, 3)
+    println(enemy)
 
+    enemy.takeDamage(4)
+    println(enemy)
+
+    enemy.takeDamage(11)
+    println(enemy)
+
+    val uglyTroll = Troll("Ugly Troll")
+    println(uglyTroll)
+    uglyTroll.takeDamage(30)
+    println(uglyTroll)
 }
