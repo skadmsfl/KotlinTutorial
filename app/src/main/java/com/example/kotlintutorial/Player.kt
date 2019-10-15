b836b2bed4d435c878d9effd3d270a958b850342
skadmsfl <noeytt.pk@gmail.com>
2019-10-15 14:55:32 +0700

    showInventory

--- app/src/main/java/com/example/kotlintutorial/Player.kt
+++ app/src/main/java/com/example/kotlintutorial/Player.kt
@@ -5,13 +5,27 @@ class Player(val name: String, var level: Int = 1, var lives: Int = 3, var score
     val inventory = ArrayList<Loot>()
 
     fun show() {
-        println("""
+        if (lives > 0) {
+            println("$name is alive")
+        } else {
+            println("$name is dead")
+        }
+    }
+
+    override fun toString(): String {
+        return """
             name: $name
             lives: $lives
             level: $level
             score: $score
             weapon: ${weapon.name}
             damage: ${weapon.damageInflicted}
-            """)
+            """
+    }
+
+    fun showInventory() {
+        println("$name's Inventory")
+        println(inventory.get(0))
+        println("================================")
     }
 }
