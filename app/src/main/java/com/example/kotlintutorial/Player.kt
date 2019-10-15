a5c384ed0fc75db468011e2d2c64a30b4030da0a
skadmsfl <noeytt.pk@gmail.com>
2019-10-15 13:59:00 +0700

    Player

---
+++ app/src/main/java/com/example/kotlintutorial/Player.kt
@@ -0,0 +1,13 @@
+package com.example.kotlintutorial
+
+class Player (val name: String, var level: Int = 1, var lives: Int = 3, var score: Int = 0) {
+
+    fun show() {
+        println("""
+            name: $name
+            lives: $lives
+            level: $level
+            score: $score
+            """)
+    }
+}
