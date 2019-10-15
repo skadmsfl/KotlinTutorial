f6d8be2e5adb4062e8a5b8ee6410235a2ac690dd
skadmsfl <noeytt.pk@gmail.com>
2019-10-15 16:02:52 +0700

    Random

--- app/src/main/java/com/example/kotlintutorial/VampyreKing.kt
+++ app/src/main/java/com/example/kotlintutorial/VampyreKing.kt
@@ -1,5 +1,7 @@
 package com.example.kotlintutorial
 
+import java.util.*
+
 class VampyreKing(name: String) : Vampyre(name) {
     init {
         hitPoints = 140
@@ -7,5 +9,19 @@ class VampyreKing(name: String) : Vampyre(name) {
 
     override fun takeDamage(damage: Int) {
         super.takeDamage(damage / 2)
+    }
+
+    fun runAway(): Boolean {
+        return lives <2
+    }
+
+    fun dodges(): Boolean {
+        val rand = Random()
+        val chance = rand.nextInt(6)
+        if (chance > 3) {
+            println("Dracula dodges")
+            return true
+        }
+        return false
     }
 }
